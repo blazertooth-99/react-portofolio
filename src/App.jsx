c9e5a49f72f80fe4ae6537c9a1ce7ec2c9057644
Lancea <christiansatrio30@gmail.com>
2024-09-03 18:45:10 +0700

    Add Daisy UI & Update Theme controller button

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Tech from "./components/Tech";
 import Experience from "./components/Experience";
 import Project from "./components/Projects";
 import Contact from "./components/Contact";
-import { IoSunny } from "react-icons/io5";
-import { IoMoon } from "react-icons/io5";
 import { useEffect, useState } from "react";
 
 export default function App() {
@@ -25,24 +23,48 @@ export default function App() {
       className="overflow-x-hidden text-neutral-600 antialiased
      selection:bg-cyan-300 selection:text-cyan-900"
     >
-      <div className="light:bg-neutral-200 dark:bg-neutral-900">
+      <div className="bg-gray-400 dark:bg-neutral-800">
+        <div className="container mx-auto px-8 bg-slate-200 dark:bg-neutral-900 rounded-2xl">
         <span className="flex flex-shrink-0 items-center justify-end px-5">
-          <button
-            onClick={() => {
+          <label className="grid cursor-pointer place-items-center">
+            <input onClick={() => {
               setDarkMode(!darkMode);
             }}
-            className="flex mr-2 mt-2 justify-center items-center dark:bg-white dark:text-black bg-black text-white px-2 py-2 border-2 border-slate-500 dark:border-black-500 rounded-full shadow
-             hover:bg-stone-700 dark:hover:bg-gray-300 font-semibold"
-          >
-            Theme :
-            <span className="ml-2 mr-1"> {darkMode ? <IoSunny /> : <IoMoon />} </span>
-          </button>
-        </span>
-
-        {/* <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(0,19,90,0.93),rgba(20,15,21,0.8))]"></div>
-      </div> */}
-        <div className="container mx-auto px-8 dark:bg-neutral-900">
+              type="checkbox"
+              value="synthwave"
+              className="toggle theme-controller bg-base-content col-span-2 col-start-1 row-start-1"
+            />
+            <svg
+              className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+            <svg
+              className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          </label>
+          </span>
           <Navbar />
           <Hero />
           <About />
@@ -54,6 +76,4 @@ export default function App() {
       </div>
     </div>
   );
-};
-
-
+}
